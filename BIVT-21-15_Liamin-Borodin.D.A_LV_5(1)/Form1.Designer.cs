e385daeb14ea2a68bdb47857b87108dcf921fe50
Даниил Лямин <Niger120404@yandex.ru>
2022-05-28 14:10:21 +0300

    Добавьте файлы проекта.

---
+++ BIVT-21-15_Liamin-Borodin.D.A_LV_5(1)/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace BIVT_21_15_Liamin_Borodin.D.A_LV_5_1_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        #region Windows Form Designer generated code
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Состояние счета:";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Счет 1:";
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(169, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Счет 2:";
+            this.label3.Click += new System.EventHandler(this.Label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 172);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Транзакции:";
+            this.label4.Click += new System.EventHandler(this.Label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 207);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Сумма";
+            this.label5.Click += new System.EventHandler(this.Label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 231);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Счет";
+            this.label6.Click += new System.EventHandler(this.Label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(7, 274);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Валюта";
+            this.label7.Click += new System.EventHandler(this.Label7_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 83);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(151, 27);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(169, 83);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(151, 27);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(73, 204);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(151, 27);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.TextChanged += new System.EventHandler(this.TextBox3_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 322);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 29);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Зачислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(169, 322);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 29);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Снять";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 119);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(151, 28);
+            this.comboBox1.TabIndex = 12;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(169, 119);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(151, 28);
+            this.comboBox2.TabIndex = 13;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.ComboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(73, 237);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(151, 28);
+            this.comboBox3.TabIndex = 14;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.ComboBox3_SelectedIndexChanged);
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(73, 271);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(151, 28);
+            this.comboBox4.TabIndex = 15;
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.ComboBox4_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(333, 371);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+    }
+}
---
+++ BIVT-21-15_Liamin-Borodin.D.A_LV_5(1)/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Windows.Forms;
+namespace BIVT_21_15_Liamin_Borodin.D.A_LV_5_1_
+{
+    public partial class Form1 : Form
+    {
+        public decimal percent_1 = 0.01M;
+        public decimal percent_2 = 0.99M;
+        private static readonly Score[] scores = new Score[2]
+        {
+            new Score("1"),
+            new Score("2")
+        };
+        private static readonly Currency[] currency = new Currency[3]
+        {
+            new Currency() { Name = "RUB", Price = 1M },
+            new Currency() { Name = "AMD", Price = 0.1574M },
+            new Currency() { Name = "CNY", Price = 11.99M}
+        };
+        private static readonly Bank_Account bank_account_1 = new Bank_Account(30000, currency[0]);
+        private static readonly Bank_Account bank_account_2 = new Bank_Account(50000, currency[0]);
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.AddRange(currency);
+            comboBox2.Items.AddRange(currency);
+            comboBox3.Items.AddRange(currency);
+            comboBox4.Items.AddRange(scores);
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+            comboBox3.SelectedIndex = 0;
+            comboBox4.SelectedIndex = 0;
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox3.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox4.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.AppendText(bank_account_1.Balance.ToString());
+            textBox2.AppendText(bank_account_2.Balance.ToString());
+            MessageBox.Show("За валютные переводы взымается комиссия 1%");
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label7_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            decimal a = bank_account_1.Balance;
+            switch (comboBox1.Text)
+            {
+                case "RUB":
+                    textBox1.Text = textBox1.Text;
+                    break;
+                case "AMD":
+                    textBox1.Text = Math.Round(a * percent_2 / currency[1].Price, 2).ToString();
+                    break;
+                case "CNY":
+                    textBox1.Clear();
+                    textBox1.Text = Math.Round(a * percent_2 / currency[2].Price, 2).ToString();
+                    break;
+            }
+        }
+
+        private void ComboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            decimal a = bank_account_2.Balance;
+            switch (comboBox2.Text)
+            {
+                case "RUB":
+                    textBox2.Text = textBox2.Text;
+                    break;
+                case "AMD":
+                    textBox2.Clear();
+                    textBox2.Text = Math.Round(a * percent_2 / currency[1].Price, 2).ToString();
+                    break;
+                case "CNY":
+                    textBox2.Clear();
+                    textBox2.Text = Math.Round(a * percent_2 / currency[2].Price, 2).ToString();
+                    break;
+            }
+        }
+
+        private void ComboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ComboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            decimal a = Convert.ToDecimal(textBox1.Text);
+            decimal b = Convert.ToDecimal(textBox2.Text);
+            decimal c = Convert.ToDecimal(textBox3.Text);
+            if (comboBox4.Text == "1")
+            {
+                switch (comboBox3.Text)
+                {
+                    case "RUB":
+                        c *= currency[0].Price;
+                        break;
+                    case "AMD":
+                        c = c * currency[1].Price * percent_1;
+                        break;
+                    case "CNY":
+                        c = c * currency[2].Price * percent_1;
+                        break;
+                }
+                decimal d = a + c;
+                textBox1.Clear();
+                textBox1.AppendText(d.ToString());
+            }
+            else if (comboBox4.Text == "2")
+            {
+                switch (comboBox3.Text)
+                {
+                    case "RUB":
+                        c *= currency[0].Price;
+                        break;
+                    case "AMD":
+                        c = c * currency[1].Price * percent_1;
+                        break;
+                    case "CNY":
+                        c = c * currency[2].Price * percent_1;
+                        break;
+                }
+                decimal d = b + c;
+                textBox2.Clear();
+                textBox2.AppendText(d.ToString());
+            }
+        }
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            decimal a = Convert.ToDecimal(textBox1.Text);
+            decimal b = Convert.ToDecimal(textBox2.Text);
+            decimal c = Convert.ToDecimal(textBox3.Text);
+            if (comboBox4.Text == "1")
+            {
+                switch (comboBox3.Text)
+                {
+                    case "RUB":
+                        c *= currency[0].Price;
+                        break;
+                    case "AMD":
+                        c = c * currency[1].Price * percent_1;
+                        break;
+                    case "CNY":
+                        c = c * currency[2].Price * percent_1;
+                        break;
+                }
+                decimal d = a - c;
+                if (d >= 0)
+                {
+                    textBox1.Clear();
+                    textBox1.AppendText(d.ToString());
+                }
+                else
+                {
+                    MessageBox.Show("У вас не достаточно средств на счету");
+                }
+            }
+            else if (comboBox4.Text == "2")
+            {
+                switch (comboBox3.Text)
+                {
+                    case "RUB":
+                        c *= currency[0].Price;
+                        break;
+                    case "AMD":
+                        c = c * currency[1].Price * percent_1;
+                        break;
+                    case "CNY":
+                        c = c * currency[2].Price * percent_1;
+                        break;
+                }
+                decimal d = b - c;
+                if (d >= 0)
+                {
+                    textBox2.Clear();
+                    textBox2.AppendText(d.ToString());
+                }
+                else
+                {
+                    MessageBox.Show("У вас не достаточно средств на счету");
+                }
+                textBox2.Clear();
+                textBox2.AppendText(d.ToString());
+
+            }
+        }
+    }
+    internal class Currency
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+        public decimal Price
+        {
+            get;
+            set;
+        }
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+    }
+    internal class Bank_Account
+    {
+        public decimal Balance;
+        public Currency Currency;
+        public Currency CurrencyDisplay
+        {
+            set
+            {
+                Currency = value;
+            }
+        }
+        public Bank_Account(decimal balance, Currency currency)
+        {
+            Balance = balance;
+            Currency = currency;
+        }
+    }
+    internal class Score
+    {
+        private readonly string Number;
+        public Score(string number)
+        {
+            Number = number;
+        }
+        public override string ToString()
+        {
+            return $"{Number}";
+        }
+    }
+}
